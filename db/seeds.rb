96b1e3794a8942b8e7eca2c09bbe07d32104acac
pratilipi <pratilipiaich@gmail.com>
2019-03-09 17:01:52 +0530

    add table and buttons

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 
 #   Character.create(name: 'Luke', movie: movies.first)
+Event.delete_all
+Participant.delete_all
 e=Event.create!({name: 'railsgirls', description:'workshop', venue: 'simpl', eventdate: Date.today, duration: 3 })
 Participant.create!([{name: 'abc', age: 30, gender: 'male', email: 'xmail@gmail.com', event_id: e.id}, 
 {name: 'abc', age: 30, gender: 'male', email: 'xmail@gmail.com', event_id: e.id}, 
 {name: 'abc', age: 30, gender: 'male', email: 'zmail@gmail.com', event_id: e.id}, 
+{name: 'xyz', age: 20, gender: 'female', email: 'ymail@gmail.com', event_id: e.id}])
+
+e=Event.create!({name: 'rails', description:'workshop', venue: 'simpl', eventdate: Date.today, duration: 3 })
+Participant.create!([{name: 'abc', age: 30, gender: 'male', email: 'xmail@gmail.com', event_id: e.id}, 
+{name: 'abc', age: 30, gender: 'male', email: 'xmail@gmail.com', event_id: e.id}, 
+{name: 'pqr', age: 30, gender: 'male', email: 'zmail@gmail.com', event_id: e.id}, 
 {name: 'xyz', age: 20, gender: 'female', email: 'ymail@gmail.com', event_id: e.id}])
